bd386add5cfb23d2350f4a954c2bb6b3af4149c3
Tyuzu-XD <102547027+tyuzu-xd@users.noreply.github.com>
2022-04-04 06:42:41 +0700

    Update build-miui.sh

--- build-miui.sh
+++ build-miui.sh
@@ -112,7 +112,7 @@ function cloneTC() {
 	PATH="${KERNEL_DIR}/aosp-clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
 	fi
         # Clone AnyKernel
-        git clone --depth=1 https://github.com/tyuzu-xd/AnyKernel3-1 -b lavender
+        git clone --depth=1 https://github.com/tyuzu-xd/AnyKernel3 -b ginkgo
 
 	}
 	
